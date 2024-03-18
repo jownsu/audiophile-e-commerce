799aaa85afb31cc54a1bc85981edd75d152400fc
Jownsu <jhonesdigno666@gmail.com>
2024-03-18 08:27:37 +0800

    Added id for sections

---
+++ app/_components/AudioGear.tsx
@@ -0,0 +1,39 @@
+import { Flex } from "@radix-ui/themes";
+
+const AudioGear = () => {
+    return (
+        <section id="audio_gear_section">
+            <Flex
+                direction={{
+                    initial: "column",
+                    md: "row"
+                }}
+                align={{
+                    md: "center"
+                }}
+                justify={{}}
+                className="mb-[118px] gap-[36px] sm:mb-[97px] sm:gap-[63px] md:gap-[30px]"
+            >
+                <div className="h-[300px] rounded-md bg-[url('/images/best-gear.jpg')] bg-cover bg-center bg-no-repeat sm:h-[320px] sm:bg-[url('/images/sm-best-gear.jpg')] md:order-2 md:h-[588px] md:w-1/2 md:bg-[url('/images/md-best-gear.jpg')]"></div>
+                <div className="text-center text-black sm:mx-auto sm:max-w-[570px] md:mx-0 md:w-1/2 md:text-start">
+                    <p className="mb-[30px] text-s8 font-bold uppercase leading-none sm:mb-[34px] sm:text-s11 md:order-1 md:max-w-[400px]">
+                        Bringing you the{" "}
+                        <span className="text-primary">best</span> audio gear
+                    </p>
+                    <p className="text-s4 opacity-50 md:max-w-[430px]">
+                        Located at the heart of New York City, Audiophile is the
+                        premier store for high end headphones, earphones,
+                        speakers, and audio accessories. We have a large
+                        showroom and luxury demonstration rooms available for
+                        you to browse and experience a wide range of our
+                        products. Stop by our store to meet some of the
+                        fantastic people who make Audiophile the best place to
+                        buy your portable audio equipment.
+                    </p>
+                </div>
+            </Flex>
+        </section>
+    );
+};
+
+export default AudioGear;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { Container, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import AudioCategories from "./_components/AudioCategories";
+import AudioGear from "./_components/AudioGear";
 
 export default function Home() {
     return (
         <div>
-            <section className="relative bg-hero bg-cover bg-center bg-no-repeat text-white md:bg-lg_hero">
+            <section
+                id="hero_section"
+                className="relative bg-hero bg-cover bg-center bg-no-repeat text-white md:bg-lg_hero"
+            >
                 <div className="absolute left-0 top-0 z-10 h-full w-full bg-black opacity-50"></div>
                 <Container className="relative z-20 px-10 text-center bg-blend-darken md:text-start">
                     <Flex
@@ -36,11 +40,14 @@ export default function Home() {
                 </Container>
             </section>
             <Container className="px-10">
-                <section className="mb-5 pb-[72px] pt-[110px] sm:pb-[48px] md:pb-[120px]">
+                <section
+                    id="audio_categories_section"
+                    className="mb-5 pb-[72px] pt-[110px] sm:pb-[48px] md:pb-[120px]"
+                >
                     <AudioCategories />
                 </section>
 
-                <section>
+                <section id="zx9_speaker_section">
                     <Flex
                         direction={{
                             initial: "column",
@@ -76,7 +83,7 @@ export default function Home() {
                     </Flex>
                 </section>
 
-                <section>
+                <section id="zx7_speaker_section">
                     <Flex
                         direction="column"
                         justify="center"
@@ -91,7 +98,7 @@ export default function Home() {
                     </Flex>
                 </section>
 
-                <section>
+                <section id="yx1_earphones_section">
                     <Flex
                         direction={{
                             initial: "column",
@@ -110,40 +117,7 @@ export default function Home() {
                         </div>
                     </Flex>
                 </section>
-
-                <section>
-                    <Flex
-                        direction={{
-                            initial: "column",
-                            md: "row"
-                        }}
-                        align={{
-                            md: "center"
-                        }}
-                        justify={{}}
-                        className="mb-[118px] gap-[36px] sm:mb-[97px] sm:gap-[63px] md:gap-[30px]"
-                    >
-                        <div className="h-[300px] rounded-md bg-[url('/images/best-gear.jpg')] bg-cover bg-center bg-no-repeat sm:h-[320px] sm:bg-[url('/images/sm-best-gear.jpg')] md:order-2 md:h-[588px] md:w-1/2 md:bg-[url('/images/md-best-gear.jpg')]"></div>
-                        <div className="text-center text-black sm:mx-auto sm:max-w-[570px] md:mx-0 md:w-1/2 md:text-start">
-                            <p className="mb-[30px] text-s8 font-bold uppercase leading-none sm:mb-[34px] sm:text-s11 md:order-1 md:max-w-[400px]">
-                                Bringing you the{" "}
-                                <span className="text-primary">best</span> audio
-                                gear
-                            </p>
-                            <p className="text-s4 opacity-50 md:max-w-[430px]">
-                                Located at the heart of New York City,
-                                Audiophile is the premier store for high end
-                                headphones, earphones, speakers, and audio
-                                accessories. We have a large showroom and luxury
-                                demonstration rooms available for you to browse
-                                and experience a wide range of our products.
-                                Stop by our store to meet some of the fantastic
-                                people who make Audiophile the best place to buy
-                                your portable audio equipment.
-                            </p>
-                        </div>
-                    </Flex>
-                </section>
+                <AudioGear />
             </Container>
         </div>
     );
